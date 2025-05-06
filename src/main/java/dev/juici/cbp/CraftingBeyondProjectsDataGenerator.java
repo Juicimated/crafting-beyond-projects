c7a80ede7abd3233d2de1cc3a883aa258c27f05a
Jaylan Smith <imjuicimated@gmail.com>
2025-05-06 14:34:41 -0400

    Initial commit

---
+++ src/main/java/dev/juici/cbp/CraftingBeyondProjectsDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.juici.cbp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CraftingBeyondProjectsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
