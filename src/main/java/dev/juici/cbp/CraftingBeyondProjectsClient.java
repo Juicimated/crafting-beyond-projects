a89dbc7e7363355bca1aafda1d0eac49a174c29c
Jaylan Smith <imjuicimated@gmail.com>
2025-05-06 14:51:51 -0400

    Project setup

---
+++ src/main/java/dev/juici/cbp/CraftingBeyondProjectsClient.java
@@ -0,0 +1,10 @@
+package dev.juici.cbp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftingBeyondProjectsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
